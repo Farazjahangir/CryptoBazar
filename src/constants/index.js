148e6e47add0282b883467f94883cb9b52665e00
Farazjahangir <jahangirfaraz98@gmail.com>
2024-06-05 21:34:26 +0500

    router added and configured

---
+++ src/constants/index.js
@@ -0,0 +1,12 @@
+const SCREEN_PATHS = {
+  HOME: "/",
+};
+
+const SCREENS_CODES = {
+    HOME: 'home'
+};
+
+export {
+    SCREENS_CODES,
+    SCREEN_PATHS
+}
---
+++ src/router/index.jsx
@@ -0,0 +1,39 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+  BrowserRouter,
+  Route,
+  Routes,
+  useLocation,
+} from "react-router-dom";
+
+import { SCREEN_PATHS, SCREENS_CODES } from "../constants";
+import { getRandomInt } from "../utils/globalHelpers";
+import Home from "../screens/Home";
+
+const Router = () => {
+  const router = createBrowserRouter([
+    {
+      path: SCREEN_PATHS.HOME,
+      element: (
+        // <ProtectedRoute screenName={SCREENS_CODES.DASHBOARD}>
+        <Home />
+        // </ProtectedRoute>
+      ),
+    },
+  ]);
+
+  return (
+    <Routes>
+      {router.routes.map((item) => (
+        <Route
+          path={item.path}
+          element={item.element}
+          key={getRandomInt(1000000, 100000000)}
+        />
+      ))}
+    </Routes>
+  );
+};
+
+export default Router;
