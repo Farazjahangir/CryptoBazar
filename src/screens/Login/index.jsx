a6fea5f78d85998b88b7afde73b8efc86aa37d6b
Farazjahangir <jahangirfaraz98@gmail.com>
2024-06-06 21:42:48 +0500

    some fixes in login and signup ui

--- src/screens/Login/index.jsx
+++ src/screens/Login/index.jsx
@@ -6,11 +6,11 @@ import Button from "../../Components/Button";
 import styles from "./style.module.scss";
 
 const Login = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const navigateTo = (path) => {
-    navigate(path)
-  }
+    navigate(path);
+  };
 
   return (
     <div className={styles.container}>
@@ -18,15 +18,18 @@ const Login = () => {
         <h1 className={styles.title}>Sign In</h1>
         <p>
           Don't have account?{" "}
-          <span className={styles.link} onClick={() => navigateTo('/signup')}>Create an account</span>
+          <span className={styles.link} onClick={() => navigateTo("/signup")}>
+            Create an account
+          </span>
         </p>
-        <div className="mt20">
-          <TextInput label="Email" inputClass={styles.input} type="email" />
-          <TextInput label="Password" type="password" />
-          <TextInput label="Password" type="password" />
-        </div>
-        <div className="mt20">
-          <Button value="Login" />
+        <div className={styles.inputContainer}>
+          <div className="mt20">
+            <TextInput label="Email" inputClass={styles.input} type="email" />
+            <TextInput label="Password" type="password" />
+          </div>
+          <div className="mt20">
+            <Button value="Login" />
+          </div>
         </div>
         <p className={styles.forgotText}>Forgot Password?</p>
       </Box>
--- src/screens/Signup/index.jsx
+++ src/screens/Signup/index.jsx
@@ -6,29 +6,42 @@ import Button from "../../Components/Button";
 import styles from "./style.module.scss";
 
 const Signup = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const navigateTo = (path) => {
-    navigate(path)
-  }
+    navigate(path);
+  };
 
   return (
     <div className={styles.container}>
       <Box className={styles.box}>
         <h1 className={styles.title}>Create An Account</h1>
         <p>
-          Already have account? <span className={styles.link} onClick={() => navigateTo('/login')}>Sign In</span>
+          Already have account?{" "}
+          <span className={styles.link} onClick={() => navigateTo("/login")}>
+            Sign In
+          </span>
         </p>
-        <div className="mt20">
-          <TextInput label="Fullname" inputClass={styles.input} type="email" />
-          <TextInput label="Contact no" inputClass={styles.input} type="email" />
-          <TextInput label="Address" inputClass={styles.input} type="email" />
-          <TextInput label="Email" inputClass={styles.input} type="email" />
-          <TextInput label="Password" type="password" />
-          <TextInput label="Confirm Password" type="password" />
-        </div>
-        <div className="mt20">
-          <Button value="Sign Up" />
+        <div className={styles.inputContainer}>
+          <div className="mt20">
+            <TextInput
+              label="Fullname"
+              inputClass={styles.input}
+              type="email"
+            />
+            <TextInput
+              label="Contact no"
+              inputClass={styles.input}
+              type="email"
+            />
+            <TextInput label="Address" inputClass={styles.input} type="email" />
+            <TextInput label="Email" inputClass={styles.input} type="email" />
+            <TextInput label="Password" type="password" />
+            <TextInput label="Confirm Password" type="password" />
+          </div>
+          <div className="mt20">
+            <Button value="Sign Up" />
+          </div>
         </div>
       </Box>
     </div>
