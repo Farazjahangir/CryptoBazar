d6be27cdf666ad54a9db4ddbe95cf5310bb3ea94
Farazjahangir <jahangirfaraz98@gmail.com>
2024-07-13 15:01:46 +0500

    some UI changes in new arrivals

--- src/screens/Home/NewArrivalSection/index.jsx
+++ src/screens/Home/NewArrivalSection/index.jsx
@@ -1,6 +1,9 @@
 import clsx from "clsx";
 
 import shoe from "../../../assets/images/shoe.jpg";
+import maleIcon from "../../../assets/icons/maleIcon.png";
+import femaleIcon from "../../../assets/icons/femaleIcon.png";
+import { Tooltip } from "@mui/material";
 import styles from "./style.module.scss";
 
 const NewArrivalSection = () => {
@@ -9,15 +12,21 @@ const NewArrivalSection = () => {
       <p className={styles.title}>New Arrival</p>
       <h1>Discover the Latest Additions</h1>
       <div className={styles.box}>
-        {[1, 2, 3, 4, 5, 6, 7, 8,9,10].map((i) => {
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((i) => {
           return (
             <div className={styles.productBox}>
               <img src={shoe} className={styles.productImg} />
               <div className={styles.detailsBox}>
-              <p className={styles.genderType}>product Type</p>
-                <p className={styles.productName}>Shoe Name</p>
-                <p className={styles.genderType}>Men</p>
-                <p className={styles.price}>50$</p>
+                <div className={styles.miniBox}>
+                  <p className={styles.productTypeText}>product Type</p>
+                  <p className={styles.price}>50$</p>
+                </div>
+                <div className={styles.miniBox}>
+                  <p className={styles.productName}>Shoe Name</p>
+                  <Tooltip title="Male" placement="top">
+                    <img src={maleIcon} className={styles.genderIcon} />
+                  </Tooltip>
+                </div>
               </div>
             </div>
           );
