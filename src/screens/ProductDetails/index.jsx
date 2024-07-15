41bab33113b281566e0d6fb9bd4a6f1bbd9db6ab
Farazjahangir <jahangirfaraz98@gmail.com>
2024-07-16 01:22:00 +0500

    product details UI done

--- src/screens/ProductDetails/index.jsx
+++ src/screens/ProductDetails/index.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import Grid from "@mui/material/Grid";
 import { CheckCircle } from "@mui/icons-material";
+import { Box, Container } from "@mui/system";
 
 import Rating from "../../Components/Rating";
 import Counter from "../../Components/Counter";
@@ -26,65 +27,67 @@ const ProductDetails = () => {
   };
 
   return (
-    <Grid container sx={{ maxWidth: 1200, margin: "0 auto" }} spacing={3}>
-      <Grid item lg={6} md={6} sm={12}>
-        <img src={shoe} className={styles.image} />
+    <Container>
+      <Grid container spacing={2}>
+        <Grid item sm={6} xs={12}>
+          <img src={shoe} className={styles.image} />
+        </Grid>
+        <Grid
+          item
+          // lg={6}
+          sm={6}
+          xs={12}
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+          }}
+        >
+          <p className={styles.categoryName}>Category</p>
+          <p className={styles.productName}>Product Name</p>
+          <div className={styles.priceBox}>
+            <p className={styles.price}>50$</p>
+            <Rating readonly={true} />
+          </div>
+          <p className={styles.productAbout}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+          <div className={styles.counterBox}>
+            <Counter
+              value={count}
+              onAdd={onAdd}
+              onMinus={onMinus}
+              onInputChange={onInputChange}
+              containerClass={styles.counterContainer}
+            />
+            <div className={styles.btnBox}>
+              <Button value="Add to Cart" />
+            </div>
+          </div>
+          <div className={styles.borderLine1} />
+          <div className={styles.info}>
+            <div className={styles.checkMarks}>
+              <CheckCircle fontSize="small" sx={{ color: "#05c46b" }} />
+              <p className={styles.text}>No-Risk Money Back Guarantee!</p>
+            </div>
+            <div className={styles.checkMarks}>
+              <CheckCircle fontSize="small" sx={{ color: "#05c46b" }} />
+              <p className={styles.text}>No Hassle Refunds</p>
+            </div>
+            <div className={styles.checkMarks}>
+              <CheckCircle fontSize="small" sx={{ color: "#05c46b" }} />
+              <p className={styles.text}>Secure Payments</p>
+            </div>
+          </div>
+        </Grid>
       </Grid>
-      <Grid
-        item
-        lg={6}
-        md={6}
-        sm={12}
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-        }}
-      >
-        <p className={styles.categoryName}>Category</p>
-        <p className={styles.productName}>Product Name</p>
-        <div className={styles.priceBox}>
-          <p className={styles.price}>50$</p>
-          <Rating readonly={true} />
-        </div>
-        <p className={styles.productAbout}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </p>
-        <div className={styles.counterBox}>
-          <Counter
-            value={count}
-            onAdd={onAdd}
-            onMinus={onMinus}
-            onInputChange={onInputChange}
-            containerClass={styles.counterContainer}
-          />
-          <div className={styles.btnBox}>
-            <Button value="Add to Cart" />
-          </div>
-        </div>
-        <div className={styles.borderLine1} />
-        <div className={styles.info}>
-          <div className={styles.checkMarks}>
-            <CheckCircle fontSize="small" />
-            <p className={styles.text}>No-Risk Money Back Guarantee!</p>
-          </div>
-          <div className={styles.checkMarks}>
-            <CheckCircle fontSize="small" />
-            <p className={styles.text}>No Hassle Refunds</p>
-          </div>
-          <div className={styles.checkMarks}>
-            <CheckCircle fontSize="small" />
-            <p className={styles.text}>Secure Payments</p>
-          </div>
-        </div>
-      </Grid>
-    </Grid>
+    </Container>
   );
 };
 
